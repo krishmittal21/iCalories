a52185b90791105a07bf1e65865d487f76286083
Krish Mittal <krishmittal212005@gmail.com>
2024-05-09 18:39:17 +0530

    data controller

--- iCalories/DataModel/DataController.swift
+++ iCalories/DataModel/DataController.swift
@@ -18,4 +18,31 @@ class DataController: ObservableObject {
             }
         }
     }
+    
+    func save(context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("Saved")
+        } catch {
+            print("not saved")
+        }
+    }
+    
+    func addFood(name: String, calories: Double, context: NSManagedObjectContext) {
+        let food = Food(context: context)
+        food.id = UUID()
+        food.date = Date()
+        food.name = name
+        food.calories = calories
+        
+        save(context: context)
+    }
+    
+    func editFood(food: Food, name: String, calories: Double, context: NSManagedObjectContext) {
+        food.date = Date()
+        food.name = name
+        food.calories = calories
+        
+        save(context: context)
+    }
 }
