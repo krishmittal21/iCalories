9a66f89158ff5cd1cfbfccc27756c7a669581888
Krish Mittal <krishmittal212005@gmail.com>
2024-05-09 19:01:29 +0530

    create

---
+++ iCalories/Views/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  iCalories
+//
+//  Created by Krish Mittal on 09/05/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedObjContext
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.date, order: .reverse)]) var food: FetchedResults<Food>
+    
+    @State private var showingAddView = false
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("\(Int(totalCaloriesToday())) Kcal Today")
+                    .foregroundStyle(.gray)
+                    .padding(.horizontal)
+                List {
+                    ForEach(food) { food in
+                        NavigationLink(destination: Text("\(food.calories)")) {
+                            HStack {
+                                VStack(alignment: .leading, spacing: 6 ) {
+                                    Text(food.name!)
+                                        .bold()
+                                    
+                                    Text("\(Int(food.calories))") + Text(" calories").foregroundStyle(.red)
+                                }
+                                Spacer()
+                                Text(calcTimeSince(date: food.date!))
+                                    .foregroundStyle(.gray)
+                                    .italic()
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteFood)
+                }
+                .listStyle(.plain)
+            }
+            .navigationTitle("iClories")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        showingAddView.toggle()
+                    } label: {
+                        Label("Add Food", systemImage: "plus.circle")
+                    }
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+            .sheet(isPresented: $showingAddView, content: {
+                AddFoodView()
+            })
+        }
+        .navigationViewStyle(.stack)
+    }
+    
+    private func totalCaloriesToday() -> Double {
+        return 0.0
+    }
+    
+    private func deleteFood(offsets: IndexSet)  {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
