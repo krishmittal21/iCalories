07100dd13fa3a28575cb48a27cbfc57475a9222b
Krish Mittal <krishmittal212005@gmail.com>
2024-05-09 15:13:09 +0530

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Krish Mittal on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
